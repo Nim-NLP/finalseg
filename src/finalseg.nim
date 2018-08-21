efcbc48d667c0dc51ff4146bebf6e32dcab53771
zhoupeng <zhoupeng@xjgreat.com>
2018-08-21 18:19:21 +0800

    still low performance

--- src/finalseg.nim
+++ src/finalseg.nim
@@ -18,22 +18,36 @@ const
         "E": "BM"
     }.toTable
 
+type
+
+    # PrevStatus = enum
+    #     B = "ES", M = "MB", S = "SE", E = "BM"
+    BMES = object
+        B,M,E,S : float
+    ProbState = tuple[prob: float, state: string]
+    ProbState2 = tuple[prob: float, state: seq[string]]
+
 template filename: string = instantiationInfo().filename
+
 let appDir = parentDir(filename())
 # var starttime =  epochTime()
 
-let prob_start = parseFile(appDir / "prob_start.json")
+let prob_start = to(parseFile(appDir / "prob_start.json"),BMES)
 let prob_trans = parseFile(appDir / "prob_trans.json")
 let prob_emit = parseFile(appDir  / "prob_emit.json")
 # echo "load json costs:",(epochTime() - starttime)
-type
-    # BMES = object
-    #     B,M,E,S : float
-    ProbState = tuple[prob: float, state: string]
-    ProbState2 = tuple[prob: float, state: seq[string]]
-var Force_Split_Words:seq[string] = @[]
 
-proc viterbi(obs:string, states:string, start_p:JsonNode, trans_p:JsonNode, emit_p:JsonNode):ProbState2 = 
+
+proc `[]`(x: BMES,index:string): float =
+  result = MIN_FLOAT
+  for name, value in x.fieldPairs:
+    if name == index:
+      return value
+  return result 
+
+var Force_Split_Words = newSeq[string]()
+
+proc viterbi(obs:string, states:string, start_p:BMES, trans_p:JsonNode, emit_p:JsonNode):ProbState2 = 
     let runeLen = obs.runeLen()
     var 
         first = initTable[string, float]()
@@ -43,24 +57,26 @@ proc viterbi(obs:string, states:string, start_p:JsonNode, trans_p:JsonNode, emit
     for k in states:  # init
         let 
             y = $k
+        let 
             y2 = runeStrAtPos(obs,0)
-            sp = if start_p.hasKey(y) : start_p[y].getFloat(MIN_FLOAT) else:MIN_FLOAT
+            sp = start_p[y]
             ep =  if emit_p[y].hasKey(y2) : emit_p[y][y2].getFloat(MIN_FLOAT) else:MIN_FLOAT
         V[0][y] =  (sp + ep)
         path[y] =  @[y]
-
+    var 
+        newpath = initTable[string, seq[string]]()
+        prob_list = newSeq[ProbState]()
     for t in 1..runeLen - 1:
         var n = initTable[string, float]()
         V.add(n)
-        var 
-            newpath = initTable[string, seq[string]]()
+        newpath.clear()
         for k in states:
             let
                 y = $k
                 y2 = runeStrAtPos(obs,t)
                 em_p = if emit_p[y].hasKey(y2) : emit_p[y][y2].getFloat( MIN_FLOAT) else:MIN_FLOAT
 
-            var a:seq[ProbState] = @[]
+            prob_list = @[]
             for y0 in PrevStatus[y]:
                 let 
                     y2 = $y0 
@@ -70,8 +86,8 @@ proc viterbi(obs:string, states:string, start_p:JsonNode, trans_p:JsonNode, emit
                     p2 = if ty.hasKey(y):ty[y].getFloat( MIN_FLOAT) else:MIN_FLOAT
                     prob = p1 + p2 + em_p
                     ps:ProbState = (prob:prob,state:y2)
-                a.add(ps)
-            let fps = max(a)
+                prob_list.add(ps)
+            let fps = max(prob_list)
             V[t][y] = fps.prob
             var
                 r = lc[y | (y <- path[fps.state]),string ]
@@ -84,14 +100,14 @@ proc viterbi(obs:string, states:string, start_p:JsonNode, trans_p:JsonNode, emit
     return r
 
 
-proc internal_cut(sentence:string):seq[string]  =
+proc internal_cut(sentence:string):seq[string] {.noInit.}  =
     # let start = cpuTime()
+    result = newSeq[string]()
     let mp = viterbi(sentence, "BMES", prob_start, prob_trans, prob_emit)
     # echo "viterbi cost:",cpuTime()-start
     var
         begin = 0
         nexti =  0
-        result = newSeq[string]()
 
     for i in 0..< sentence.runeLen()  :
         let pos = mp.state[i]
@@ -106,35 +122,37 @@ proc internal_cut(sentence:string):seq[string]  =
             nexti = i + 1
     if nexti < sentence.runeLen():
         result.add( runeSubStr(sentence,nexti,sentence.runeLen()-nexti))
-    return result
 
- 
 let
-    re_han = re(r"(*UTF)([\x{4E00}-\x{9FD5}]+)")
+    # re_han = re(r"(*UTF)([\x{4E00}-\x{9FD5}]+)")
+    re_han = re(r"(*UTF)([\p{Han}]+)")
     re_skip = re(r"([a-zA-Z0-9]+(?:\.\d+)?%?)")
 
 proc add_force_split*(word:string) = 
     Force_Split_Words.add(word)
 
-proc cut*(sentence:string):seq[string] {.discardable.} = 
-    var result = newSeq[string]()
+proc cut*(sentence:string):seq[string] {.discardable,noInit.} = 
+    result = newSeq[string]()
     if sentence.runeLen() == 0:
         return result
     let blocks:seq[string] = nre.split(sentence,re_han)
-    
+    var 
+        sl = newSeq[string]()
+        tmp = newSeq[string]()
+        wordStr = ""
     for blk in blocks:
         if isSome(blk.match(re_han)) == true:
-            let sl:seq[string] = internal_cut(blk)
+            sl = internal_cut(blk)
             for word in sl:
-                let wordStr = $word
+                wordStr = $word
                 if (wordStr in Force_Split_Words == false):
                     result.add( wordStr)
                 else:
                     for c in wordStr:
                         result.add( $c )
         else:
-            let tmp = split(blk,re_skip)
+            tmp = split(blk,re_skip)
             for x in tmp:
-                if x.runeLen()>0:
+                if sentence.len == 0 and x.runeLen()>0:
                     result.add( x)
     return result
