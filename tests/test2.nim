f7c09b333a061a3ccca9d082919062af4f580f3b
zhoupeng <zhoupeng@xjgreat.com>
2018-08-22 22:57:54 +0800

    o

--- tests/test2.nim
+++ tests/test2.nim
@@ -6,26 +6,27 @@ import strutils
 # import nimprof 
 # wget https://raw.githubusercontent.com/yanyiwu/practice/master/nodejs/nodejieba/performance/weicheng.utf8 -O tests/weicheng.utf8
 proc main =
-    # var lines:seq[string] = newSeq[string]()
+    var lines:seq[string] = newSeq[string]()
 
     let appDir = getCurrentDir()
 
     let weicheng = appDir / "tests" / "weicheng.utf8"
 
-    # var 
-    #     fs = newFileStream(weicheng, fmRead)
-    #     line = ""
-    # if not isNil(fs):
-    #     while fs.readLine(line):
-    #         lines.add(line)
-    #     fs.close()
+    var 
+        fs = newFileStream(weicheng, fmRead)
+        line = ""
+    if not isNil(fs):
+        while fs.readLine(line):
+            lines.add(line)
+        fs.close()
 
     # var result:seq[string] = @[]
-
-    var starttime = epochTime()
-
+    # var content = readFile(weicheng)
+    var 
+        starttime = epochTime()
     # for i in 0..49:
-    for line in splitLines(readFile(weicheng)):
+        # splitLines(content):
+    for line in lines:
 
         # let cuted = cut(line)
 
