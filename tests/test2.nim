92bb816f927db789579477748bf7406da754da8e
zhoupeng <zhoupeng@xjgreat.com>
2018-08-22 22:41:42 +0800

    o

--- tests/test2.nim
+++ tests/test2.nim
@@ -5,31 +5,35 @@ import finalseg
 import strutils
 # import nimprof 
 # wget https://raw.githubusercontent.com/yanyiwu/practice/master/nodejs/nodejieba/performance/weicheng.utf8 -O tests/weicheng.utf8
-var lines:seq[string] = newSeq[string]()
+proc main =
+    # var lines:seq[string] = newSeq[string]()
 
-let appDir = getCurrentDir()
+    let appDir = getCurrentDir()
 
-let weicheng = appDir / "tests" / "weicheng.utf8"
+    let weicheng = appDir / "tests" / "weicheng.utf8"
 
-var 
-    fs = newFileStream(weicheng, fmRead)
-    line = ""
-if not isNil(fs):
-    while fs.readLine(line):
-        lines.add(line)
-    fs.close()
+    # var 
+    #     fs = newFileStream(weicheng, fmRead)
+    #     line = ""
+    # if not isNil(fs):
+    #     while fs.readLine(line):
+    #         lines.add(line)
+    #     fs.close()
 
-# var result:seq[string] = @[]
+    # var result:seq[string] = @[]
 
-var starttime = epochTime()
+    var starttime = epochTime()
 
-# for i in 0..49:
-for line in lines:
+    # for i in 0..49:
+    for line in splitLines(readFile(weicheng)):
 
-    # let cuted = cut(line)
+        # let cuted = cut(line)
 
-    discard lcut(line).join("/")
-    # result[random.randint(0, 9)] = r
-    #result[random.randint(0, 9)] = jieba.cut(line)
-var endtime =  epochTime()
-echo (endtime - starttime)
+        discard lcut(line).join("/")
+        # result[random.randint(0, 9)] = r
+        #result[random.randint(0, 9)] = jieba.cut(line)
+    var endtime =  epochTime()
+    echo (endtime - starttime)
+
+when isMainModule:
+    main()
